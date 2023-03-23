a368ba05285ec4fdc7c5939458e8bee52139efba
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-03-23 09:26:29 +0000

    finalizado

--- src/app.py
+++ src/app.py
@@ -6,16 +6,13 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -28,16 +25,12 @@ def sitemap():
 def handle_hello():
     members = jackson_family.get_all_members()
     response_body = members
-    
     return jsonify(response_body), 200
 
 @app.route('/member/<int:member_id>', methods=['GET'])
 def get_member_by_id(member_id):
     member = jackson_family.get_member(member_id)
-    response_body = {
-        "hello": "world",
-        "member": member
-    }
+    response_body = member
     return jsonify(response_body), 200
 
 @app.route('/member', methods=['POST'])
@@ -48,22 +41,22 @@ def create_member():
     body_id = request.json.get("id")
     jackson_family.add_member({
         "id": body_id if body_id is not None else jackson_family._generateId(),
-        "first_name": body_first_name,
         "last_name": jackson_family.last_name,
+        "first_name": body_first_name,
         "age": body_age,
         "lucky_numbers": body_lucky_numbers,
     })
+
     response_body = {
-    
+        "hello": "world"
     }
-    
     return jsonify(response_body), 200
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member_by_id(member_id):
-    member = jackson_family.delete_member(member_id)
+    jackson_family.delete_member(member_id)
     response_body = {
-        "done": True,
+        "done": True
     }
     return jsonify(response_body), 200
 
